3de2b67c5fc3f60edd61bc7a6d3a289d09111838
Yunus <yunushaznedar@gmail.com>
2017-08-22 20:18:30 +0300

    Music Fragment

--- app/src/main/java/example/yunus/a13reasonswhy/Fragments/MusicFragment.java
+++ app/src/main/java/example/yunus/a13reasonswhy/Fragments/MusicFragment.java
@@ -1,7 +1,10 @@
 package example.yunus.a13reasonswhy.Fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,16 +18,21 @@ import example.yunus.a13reasonswhy.R;
 public class MusicFragment extends Fragment {
 
 
-    public MusicFragment() {
+    public MusicFragment()
+    {
         // Required empty public constructor
     }
-
-
+    //https://open.spotify.com/user/11131679308
+    //https://open.spotify.com/user/11131679308/playlist/3NDM5KSpHC6R2tgRAVSsUX
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false);
+        View view= inflater.inflate(R.layout.fragment_music, container, false);
+        String uri = "spotify:track:5aW6mgQmHPszfK1wyonSZi";
+        Intent launcher = new Intent( Intent.ACTION_VIEW, Uri.parse(uri) );
+        startActivity(launcher);
+        return view;
     }
 
 }
